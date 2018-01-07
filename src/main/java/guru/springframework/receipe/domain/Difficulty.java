74af57a68adf19373d31440fb5a18523316a864b
PALLAB <pallab123ban@gmail.com>
2018-01-06 23:20:49 -0600

    added enum difficulty and defined the reln between recipe and difficulty

---
+++ src/main/java/guru/springframework/receipe/domain/Difficulty.java
@@ -0,0 +1,10 @@
+package guru.springframework.receipe.domain;
+
+/**
+ * This is the difficulty enumeration
+ * @author PALLAB
+ */
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/guru/springframework/receipe/domain/Recipe.java
+++ src/main/java/guru/springframework/receipe/domain/Recipe.java
@@ -29,8 +29,8 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
-    // to do
-    //private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -121,5 +121,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
